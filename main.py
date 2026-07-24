ad569c0ae178c0a08f2cac65c38d692d12d2004e
Sergio Augusto <mac-sa@live.com>
2026-07-24 12:05:58 -0600

    Fase 1: POST guarda en lista, GET lista tareas

---
+++ main.py
@@ -0,0 +1,42 @@
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class Tarea(BaseModel):
+    titulo: str
+    descripcion: str
+    hecho: bool
+
+tareas = []
+
+
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return{"mensaje": "hola"}
+
+
+@app.get("/saludo/{nombre}")
+def saludar(nombre):
+    return{"mensaje": f"hola {nombre} que tal tu dia"}
+
+@app.get("/buscar")
+def buscando_en(ciudad):
+    return{"buscando en": f"la ciudad de {ciudad}"}
+
+
+@app.get("/usuarios/{user_id}/productos")
+def funcion(user_id, categoria):
+    return{"usuario": user_id,  "categoria": categoria }
+
+
+@app.post ("/tareas" , status_code=201)
+def crear_tarea(tarea : Tarea ):
+    tareas .append( tarea)
+    return tarea 
+
+@app.get("/tareas")
+def listar_tareas():
+    return tareas
