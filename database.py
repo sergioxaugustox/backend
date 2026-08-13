3c83e2f3331e0d0c410c6d8de26232658e8076c4
Sergio Augusto <mac-sa@live.com>
2026-08-13 16:46:32 -0600

    Fase 3: esquema APU - insumos, conceptos, composicion con FK

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
 
 load_dotenv()
 
@@ -22,3 +22,25 @@ class Tarea(Base):
     descripcion = Column(String)
     hecho = Column(Boolean, default=False)
 
+class Insumo(Base):
+    __tablename__ = "insumos" 
+    id = Column(Integer,primary_key=True, index=True)
+    nombre = Column(String, nullable=False)
+    unidad = Column(String, nullable=False)   
+    tipo = Column(String, nullable=False)
+
+class Concepto(Base):
+    __tablename__ = "conceptos"
+    id = Column(Integer,primary_key=True, index=True )
+    codigo = Column(String, nullable=False)
+    descripcion = Column(String, nullable=False)
+    unidad = Column(String, nullable=False)
+
+class Composicion(Base):
+    __tablename__ = "composicion"
+    id = Column(Integer, primary_key=True, index=True)
+    concepto_id = Column(Integer, ForeignKey("conceptos.id"), nullable=False)
+    insumo_id = Column(Integer, ForeignKey("insumos.id"), nullable=False)
+    cantidad = Column(Float, nullable=False)
+
+Base.metadata.create_all(bind=engine)
